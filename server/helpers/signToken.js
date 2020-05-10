61ab15dc029488f33ec2099632af5747fd85f463
Diablobait <thor.stg@gmail.com>
2020-05-10 16:40:48 +0200

    🔑 Remove username and email on token

--- server/helpers/signToken.js
+++ server/helpers/signToken.js
@@ -1,8 +1,9 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = (payload) => {
+  const { id } = payload;
   return( 
-    jwt.sign(payload, process.env.SECRETKEY, {
+    jwt.sign(id, process.env.SECRETKEY, {
       expiresIn: process.env.TOKENEXPIRATION
     }))
 };
